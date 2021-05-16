67bae99cc2dd0bcb40755c3dccf4ea181a9ee26a
saugatdhimal <dhimalsaugat77@gmail.com>
2021-05-16 10:55:24 +0545

    👨🏻‍💻👨🏻‍💻👨🏻‍💻 adding scss & navbar stuff 👨🏻‍💻👨🏻‍💻👨🏻‍💻

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,25 @@
-
+import Link from "next/link";
+import styles from "../styles/Nav.module.scss";
 function Navbar() {
-    return (
-        <div>
-            <h1>I am Navbar</h1>
+  return (
+    <nav>
+      <div className={styles.nav}>
+        <div className={styles.navLeft}>
+          <Link href="/">
+            <a><img src="/Pasal.png" /></a>
+          </Link>
         </div>
-    )
+        <div className={styles.navRight}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/product">
+            <a>Product</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
