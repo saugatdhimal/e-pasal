ec922f6f9135801f12eca0ce27b3ee2fd4cbc172
saugatdhimal <dhimalsaugat77@gmail.com>
2021-05-16 14:42:33 +0545

    models & productSchema for products 🔥🔥🔥

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import initDB from "../../helpers/initDB"
+import initDB from "../../helpers/initDB";
+import Product from "../../models/Product";
+initDB();
 
-initDB()
 export default (req, res) => {
-  res.status(200).json({ name: 'Pasal' })
-}
+  Product.find().then((data) => res.status(200).json(data));
+};
